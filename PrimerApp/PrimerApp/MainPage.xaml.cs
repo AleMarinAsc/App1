2c13bdf02a468405351647f1ed4e7a5ce823df21
AleMarinAsc <ale.m.ascencio@hotmail.com>
2021-09-17 14:29:02 -0500

    Agregar archivos de proyecto.

---
+++ PrimerApp/PrimerApp/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PrimerApp
+{
+    public partial class MainPage : ContentPage
+    {
+
+        int _count;
+        string _message;
+        Button button1;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            _count = 0;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (button1 == null)
+                button1 = ((Button)sender);
+
+            _count++;
+            _message = "You cliked me {0} times";
+            button1.Text = string.Format(_message, _count);
+        }
+
+        private void Reset_Clicked(object sender, EventArgs e)
+        {
+            _message = "Click Me";
+            _count = 0;
+            if(button1 != null)
+            {
+                button1.Text = _message;
+            }
+        }
+    }
+}
